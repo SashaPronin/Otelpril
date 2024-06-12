ca5e7f113189653625bc9d40165b4c5190958a39
daina <email@address>
2024-06-12 21:46:52 +0700

    *

--- OtelPrilNew/Pages/HomePage.xaml.cs
+++ OtelPrilNew/Pages/HomePage.xaml.cs
@@ -6,7 +6,7 @@ public partial class HomePage : ContentPage
 
     private async void OnImageButtonClicked(object sender, EventArgs e)
     {
-        var nextPage = new NextPage();
+        var nextPage = new NextPage(StarOne);
         await Navigation.PushAsync(nextPage);
     }
 }
